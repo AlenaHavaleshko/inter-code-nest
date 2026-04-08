5670bc31966a467887ab57db1072e6f051ab0cea
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2026-04-08 12:00:01 +0300

    delete .spec files

--- backend/src/candidates/candidates.module.ts
+++ backend/src/candidates/candidates.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CandidatesService } from './candidates.service';
 import { CandidatesController } from './candidates.controller';
 
+// module
 @Module({
   controllers: [CandidatesController],
   providers: [CandidatesService],
